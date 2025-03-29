e62181fb5f2f9627fee3b642aa26bce86f091ac5
tmanikanta05 <tummalamanikanta05@gmail.com>
2025-03-29 15:13:58 +0530

    Add files via upload

---
+++ commandline.java
@@ -0,0 +1,12 @@
+class commandline
+{
+public static void main(String args[])
+{
+String s1=args[0];
+String s2=args[1];
+String s3=args[2];
+System.out.println("Name:" +s1);
+System.out.println("Rno:" +s2);
+System.out.println("Dept:" +s3);
+}
+}
